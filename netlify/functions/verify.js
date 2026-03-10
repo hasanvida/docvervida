e1825c53f0672658091a70d495c1b18cb396e0c4
hasanvida <hasan@vida.id>
2026-03-10 11:06:54 +0700

    Update verify.js

--- netlify/functions/verify.js
+++ netlify/functions/verify.js
@@ -1,1 +1,88 @@
+const { v4: uuidv4 } = require("uuid")
 
+exports.handler = async (event) => {
+
+try{
+
+const body = JSON.parse(event.body)
+const image = body.image
+
+// generate values
+const partnerTrxId = uuidv4()
+const groupId = uuidv4()
+const epoch = Math.floor(Date.now()/1000)
+
+
+// GET TOKEN
+
+const tokenRes = await fetch(
+"https://qa-sso.vida.id/auth/realms/vida/protocol/openid-connect/token",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/x-www-form-urlencoded"
+},
+body:new URLSearchParams({
+grant_type:"client_credentials",
+client_id:process.env.VIDA_CLIENT_ID,
+client_secret:process.env.VIDA_CLIENT_SECRET
+})
+}
+)
+
+const tokenData = await tokenRes.json()
+
+const accessToken = tokenData.access_token
+
+
+// CALL VERIFY API
+
+const verifyRes = await fetch(
+"https://my-services-sandbox.np.vida.id/api/v2/verify",
+{
+method:"POST",
+headers:{
+Authorization:`Bearer ${accessToken}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+
+operations:["ocr","idVerification"],
+
+payload:{
+partnerTrxId,
+groupId,
+idType:"ID_CARD",
+country:"MYS",
+idSubtype:"MyKad",
+idFrontSideImage:image
+},
+
+userConsent:{
+userIP:"0.0.0.0",
+country:"IDN",
+obtained:true,
+obtainedAt:epoch
+}
+
+})
+}
+)
+
+const verifyData = await verifyRes.json()
+
+return{
+statusCode:200,
+body:JSON.stringify(verifyData)
+}
+
+}catch(err){
+
+return{
+statusCode:500,
+body:JSON.stringify({error:err.message})
+}
+
+}
+
+}
